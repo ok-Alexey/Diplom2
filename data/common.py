fe9ec1b4a618bc2be420ecea52a42b1c369e1073
Alexey <Alex.rs.r.s@yandex.ru>
2025-11-23 21:19:56 +0300

    0

--- data/common.py
+++
@@ -1,2 +0,0 @@
-class MainUrls: # CommonUrls
-    URL_STELLARBURGERS_API = "https://stellarburgers.education-services.ru/api" # MAIN_API_URL
--- data/order.py
+++
@@ -1,9 +0,0 @@
-from data.common import MainUrls
-
-
-class InfoAboutOrder:
-    URL_FOR_CREATE_ORDER = (f"{MainUrls.URL_STELLARBURGERS_API}/orders", "POST")
-    NO_INGREDIENTS_ARE_INCLUDED_IN_THE_ORDER = {
-        "success": False,
-        "message": "Ingredient ids must be provided",
-    }
